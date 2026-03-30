1364e0b8da84c9b27bc41cbade9ed5fe09e880ba
goodlogicphysics@gmail.com <goodlogicphysics@gmail.com>
2026-03-30 15:13:53 +0400

    Update v0.01 (Adding gravity showing objects with numbers without graphics)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
 
-class Physics
+const double g = 9.8;
+
+class PhysicalObject
 {
+private:
 public:
-  double calcGravity(double height);
-}
+  double calcGravity(double* height,double time);
+  bool isGravObj;
+};
 
 int main()
 {
+  double i = 0.0;
+  double height;
+  double* ptrHeight = 0;
+  std::cout << "Write object that downing from height -> ";
+  std::cin >> height;
+  ptrHeight = &height;
+  double time;
+  std::cout << "Write object downing time -> ";
+  std::cin >> time;
+  PhysicalObject objGravity;
+  objGravity.isGravObj = true;
+  while(i <= time)
+  {
+    objGravity.calcGravity(ptrHeight,i);
+    std::cout << "Height -> " << height << std::endl;
+    std::cout << "Time -> " << i << std::endl;
+    i = i + 1.0;
+  }
+  return 0;
+}
+
+double PhysicalObject::calcGravity(double* const ptrHeight,double time)
+{
+  if(isGravObj)
+  {
+    *ptrHeight = *ptrHeight - (0.5*g*time*time);
+    return *ptrHeight;
+  }
+  else
+  {
+    return 1;
+  }
   return 0;
 }
